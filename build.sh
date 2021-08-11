79ec0f9ee0289426ddc42ce3d08912c1d1cdf62e
James Liu <2211002+jamesliu96@users.noreply.github.com>
2021-08-11 11:46:11 +0000

    feat: add darwin/arm64 build

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ rm -rf build
 pkg=github.com/jamesliu96/geheim/cmd/ghm
 GOOS=darwin GOARCH=amd64 \
   go build -trimpath -o build/ghm_darwin_amd64 $pkg
+GOOS=darwin GOARCH=arm64 \
+  go build -trimpath -o build/ghm_darwin_arm64 $pkg
 GOOS=linux GOARCH=amd64 \
   go build -trimpath -o build/ghm_linux_amd64 $pkg
 GOOS=windows GOARCH=amd64 \
