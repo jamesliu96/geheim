c4456c4b5e8bf6e75d9f65c89a43556ae128b51a
James Liu <2211002+jamesliu96@users.noreply.github.com>
2021-09-09 10:06:03 +0000

    build: stripped binaries

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf build
 
 tag=$(git describe --tags --always)
 rev=$(git rev-list -1 HEAD)
-ldflags="-X main.gitTag=$tag -X main.gitRev=$rev"
+ldflags="-X main.gitTag=$tag -X main.gitRev=$rev -s -w"
 pkg=github.com/jamesliu96/geheim/cmd/ghm
 osarchs=(
   "darwin amd64"
