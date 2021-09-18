0fb420832c862c5bdf013a74e0b4b0c1ec096d62
James Liu <2211002+jamesliu96@users.noreply.github.com>
2021-09-18 10:00:36 +0000

    build: integrate build script

--- build.sh
+++ build.sh
@@ -2,33 +2,37 @@
 
 set -e
 
-rm -rf build
-
 tag=$(git describe --tags --always)
 rev=$(git rev-list -1 HEAD)
-ldflags="-X main.gitTag=$tag -X main.gitRev=$rev -s -w"
 pkg=github.com/jamesliu96/geheim/cmd/ghm
-osarchs=(
-  "darwin amd64"
-  "darwin arm64"
-  "linux amd64"
-  "linux arm64"
-  "windows amd64"
-  "windows arm64"
-  "js wasm"
-)
+ldflags="-X main.gitTag=$tag -X main.gitRev=$rev"
 
-for i in "${osarchs[@]}"; do
-  osarch=($i)
-  os=${osarch[0]}
-  arch=${osarch[1]}
-  suffix=""
-  if [[ $os = "windows" ]]; then
-    suffix=".exe"
-  fi
-  if [[ $arch = "wasm" ]]; then
-    suffix=".wasm"
-  fi
-  GOOS=$os GOARCH=$arch \
-    go build -trimpath -ldflags="$ldflags" -o build/ghm_${os}_${arch}${suffix} $pkg
-done
+if [[ $1 = "dist" ]]; then
+  rm -rf dist
+  ldflags="$ldflags -s -w"
+  osarchs=(
+    "darwin amd64"
+    "darwin arm64"
+    "linux amd64"
+    "linux arm64"
+    "windows amd64"
+    "windows arm64"
+    "js wasm"
+  )
+  for i in "${osarchs[@]}"; do
+    osarch=($i)
+    os=${osarch[0]}
+    arch=${osarch[1]}
+    suffix=""
+    if [[ $os = "windows" ]]; then
+      suffix=".exe"
+    fi
+    if [[ $arch = "wasm" ]]; then
+      suffix=".wasm"
+    fi
+    GOOS=$os GOARCH=$arch \
+      go build -trimpath -ldflags="$ldflags" -o dist/ghm_${os}_${arch}${suffix} $pkg
+  done
+else
+  go run -trimpath -ldflags="$ldflags" $pkg $@
+fi
