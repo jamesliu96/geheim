ebc0a49f304d471301597574f1f63d0ce4dda7ab
James Liu <jamesliu96@users.noreply.github.com>
2023-02-08 16:05:00 +0800

    build: omit openbsd/mips64

--- _build.sh
+++ _build.sh
@@ -20,6 +20,7 @@ if [[ $1 = "-build" ]]; then
     arch=${osarch[1]}
     suffix=
     [[ $os = "android" || $os = "ios" ]] && continue
+    [[ $os = "openbsd" && $arch = "mips64" ]] && continue
     [[ $os = "windows" ]] && suffix=".exe"
     [[ $arch = "wasm" ]] && suffix=".wasm"
     out="${outdir}/${app}_${os}_$arch$suffix"
--- go.mod
+++ go.mod
@@ -7,5 +7,3 @@ require (
 	golang.org/x/sys v0.5.0
 	golang.org/x/term v0.5.0
 )
-
-replace golang.org/x/sys v0.5.0 => golang.org/x/sys v0.3.0
